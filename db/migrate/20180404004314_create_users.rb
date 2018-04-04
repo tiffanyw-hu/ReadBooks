be049d2cdf89d5a9400dcbe963957be9853f25b7
Andrew Tae <tiffanyw.hu@gmail.com>
2018-04-03 21:49:12 -0700

    Setup

---
+++ db/migrate/20180404004314_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :img_url
+      t.string :password_digest
+      t.string :session_token
+
+      t.timestamps
+    end
+  end
+end
