62597ee50c0c3fb4de85496076c2b07419e9650d
Andrew Tae <tiffanyw.hu@gmail.com>
2018-04-03 22:56:13 -0700

    Prepare for initial Heroku push

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: {format: :json} do
+    resource :user, only: [:create]
+    resource :session, only: [:create, :destroy, :show]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
