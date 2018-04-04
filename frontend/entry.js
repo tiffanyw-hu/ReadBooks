3e6fe33e5370a532f598d7781d90ffb76e43aeef
Andrew Tae <tiffanyw.hu@gmail.com>
2018-04-03 21:49:12 -0700

    Setup

---
+++ frontend/entry.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// //Components
+// import Root from './components/root';
+import configureStore from './store/store';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>ReadBooks</h1>, root);
+});
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+let path = require('path');
+
+module.exports = {
+  entry: './frontend/entry.jsx',
+  output: {
+    filename: './app/assets/javascripts',
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
