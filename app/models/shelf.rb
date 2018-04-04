70f23b58902b8b62db1c22fc0845270f9823d557
Andrew Tae <tiffanyw.hu@gmail.com>
2018-04-04 10:16:29 -0700

    Finished book shelf shelving models

--- app/models/shelf.rb
+++ app/models/shelf.rb
@@ -1,8 +1,13 @@
 class Shelf < ApplicationRecord
 
-  validates: :name, presence: true
-  validates: :name, uniqueness: true
+  validates :name, presence: true
+  validates :name, uniqueness: true
 
   belongs_to :user
-  
+  has_many :shelvings
+    foreign_key: :shelf_id,
+    class_name: :Shelving
+  has_many :books
+    through: :shelvings
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404154824) do
+ActiveRecord::Schema.define(version: 20180404170733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.string "img_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "description"
+    t.index ["author"], name: "index_books_on_author"
+    t.index ["title"], name: "index_books_on_title"
+  end
+
   create_table "shelves", force: :cascade do |t|
     t.string "name"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shelvings", force: :cascade do |t|
+    t.integer "book_id"
+    t.integer "shelf_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
