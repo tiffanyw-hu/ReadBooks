d0b4f7290ca069a82c827b52effe328cd922bc45
Andrew Tae <tiffanyw.hu@gmail.com>
2018-04-04 09:37:40 -0700

    Add shelf model and controller

---
+++ app/controllers/api/shelves_controller.rb
@@ -0,0 +1,47 @@
+class Api::ShelvesController < ApplicationController
+
+  before_action :require_logged_in
+
+  def create
+    @shelf = Shelf.new(shelf_params)
+
+    if @shelf.save
+      render "api/users/show"
+    else
+      render json: @shelf.errors.full_message, status: 422
+    end
+
+  end
+
+  def update
+    @shelf = Shelf.find(params[:id])
+
+    if @shelf.update(shelf_params)
+      render "api/users/show"
+    else
+      render json: @shelf.errors.full_message, status: 422
+    end
+    
+  end
+
+  def destroy
+    @shelf = Shelf.find(params[:id])
+    @shelf.destroy
+    render "api/users/show"
+  end
+
+  def index
+    @shelves = Shelf.all
+  end
+
+  def show
+    @shelf = Shelf.find(params[:id])
+  end
+
+  private
+
+  def shelf_params
+    params.require(:shelf).permit(:name, :user_id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
     resource :session, only: [:create, :destroy, :show]
   end
 
+  root "static_pages#root"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
