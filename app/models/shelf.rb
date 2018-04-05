fde33b14e1b8f89548c54e71164d40aa7b071fa3
Andrew Tae <tiffanyw.hu@gmail.com>
2018-04-05 11:27:05 -0700

    Update shelf

--- app/models/shelf.rb
+++ app/models/shelf.rb
@@ -3,7 +3,7 @@ class Shelf < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
 
-  belongs_to :user,
+  belongs_to :user
   has_many :shelvings,
     foreign_key: :shelf_id,
     class_name: :Shelving
