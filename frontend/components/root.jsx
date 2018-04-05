11e2f116bf09b2383d1862e363cd465b0d31c735
Andrew Tae <tiffanyw.hu@gmail.com>
2018-04-04 17:03:52 -0700

    Add User Views

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import SignUpFormContainer from './sessions/signup_form_container';
-
+import SignInFormContainer from './sessions/signin_form_container';
 
 const Root = ({store}) => (
   <Provider store={store}>
-    <div>  
+    <div>
     <p>Render</p>
     <SignUpFormContainer />
+    <SignInFormContainer />
   </div>
   </Provider>
 );
--- frontend/components/sessions/signin_form_container.jsx
+++ frontend/components/sessions/signin_form_container.jsx
@@ -1,3 +1,19 @@
 import {connect} from 'react-redux';
 import React from 'react';
-import {signin}
+import {signin} from '../../actions/session_actions';
+import SessionForm from './session_form';
+
+const mapStateToProps = (state, ownProps) => ({
+  formType: "signin",
+  currentUser: state.session.currentUser,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  action: (user) => dispatch(signin(user))
+});
+
+const SignInFormContainer = connect(
+  mapStateToProps, mapDispatchToProps
+)(SessionForm);
+
+export default SignInFormContainer;
