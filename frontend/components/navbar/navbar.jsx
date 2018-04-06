5e0ff47ed8a407b4a34bcfb752c51d4903007cb1
Andrew Tae <tiffanyw.hu@gmail.com>
2018-04-05 17:29:18 -0700

    YOU CAN NEVER LEAVE THIS SITE but you can if you refresh

---
+++ frontend/components/navbar/navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const sessionLinks = () => (
+  <nav className="login-signup">
+    <Link to="/login">Login</Link>
+    <Link to="/signup">Sign up!</Link>
+  </nav>
+);
+
+const personalGreeting = (currentUser, logout) => (
+	<hgroup className="header-group">
+    <h2 className="header-name">Hi, {currentUser.username}!</h2>
+    <button className="header-button" onClick={logout}>Log Out</button>
+	</hgroup>
+);
+
+const navBar = ({ currentUser, logout }) => (
+  currentUser ? personalGreeting(currentUser, logout) : sessionLinks()
+);
+
+export default navBar;
