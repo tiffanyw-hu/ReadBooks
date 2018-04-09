291a33791c52d96eb010887659982058af67d0d7
Andrew Tae <tiffanyw.hu@gmail.com>
2018-04-09 11:23:31 -0700

    Fixed a currentUser greeting page

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -7,6 +7,7 @@ import BooksIndexContainer from './books/books_index_container';
 class Splash extends React.Component {
 
   render() {
+    // <img src="http://res.cloudinary.com/dqj3kgpoj/image/upload/v1523297820/background.png" />
     return (
       <div className="splash">
         <div className="splash-signin">
