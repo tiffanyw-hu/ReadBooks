ddd388250ace63ecd178e614622d64b8f222d669
Andrew Tae <tiffanyw.hu@gmail.com>
2018-04-10 17:36:26 -0700

    Finish Auth CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
-  get 'reviews/show'
-
-  get 'reviews/create'
-
-  get 'reviews/update'
-
-  get 'reviews/destroy'
 
   namespace :api, defaults: {format: :json} do
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy, :show]
-    resources :books, only: [:index, :show]
+    resources :books, only: [:index, :show] do
+      resources :reviews, only: [:show, :create, :update, :destroy]
+    end 
     resources :shelves, only: [:index, :show, :create, :update, :destroy]
     resources :shelvings, only: [:create, :destroy]
     resources :reviews, only: [:show, :create, :udpate, :destroy]
--- frontend/components/books/book_show.jsx
+++ frontend/components/books/book_show.jsx
@@ -4,22 +4,39 @@ class BookShow extends React.Component {
 
   constructor(props) {
     super(props);
+    // this.state = {showmore: false}
   }
 
   componentDidMount() {
     this.props.fetchBook(this.props.match.params.book_id)
   }
 
+  // renderDescription() {
+  //   let description = this.props.book.description;
+  //   if (!this.state.showmore) {
+  //     description = description.slice(0, )
+  //     what determines the length
+  //   }
+  // }
+
   render () {
     if (!this.props.book) return null
 
     return (
-    <div className="book-details">
-      <h3 className="book-title">{this.props.book.title}</h3>
-      <p className="book-author">{this.props.book.author}</p>
-      <p className="book-description">{this.props.book.description}</p>
-      <img src={this.props.book.img_url} width="150px" height="231px" />
-    </div>
+      <div className="bookshowpage-container">
+        <div className="page-top">
+          <div className="img-col">
+            <img className="book-img" src={this.props.book.img_url} width="150px" height="231px" />
+          </div>
+          <div className="book-details">
+            <h3 className="book-title">{this.props.book.title}</h3>
+            <p className="book-author">{this.props.book.author}</p>
+            <div>W</div>
+            <p className="book-description">{this.props.book.description}</p>
+            <hr className="hr"></hr>
+          </div>
+        </div>
+      </div>
     )
   }
 
