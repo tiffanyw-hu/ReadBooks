a00cac418a0e08feedd4ad3fd0da933ccbb81c16
Andrew Tae <tiffanyw.hu@gmail.com>
2018-04-11 09:19:50 -0700

    Other Heroku Push

--- frontend/actions/review_actions.js
+++ frontend/actions/review_actions.js
@@ -1,36 +1,18 @@
 import * as ReviewAPIUtil from '../utils/review_api_util';
-
-export const RECEIVE_REVIEWS = 'RECEIVE-REVIEWS';
-export const RECEIVE_REVIEW = 'RECEIVE_REVIEW';
-export const REMOVE_REVIEW = 'REMOVE_REVIEW';
-
-export const receiveReviews = reviews => ({
-  type: RECEIVE_REVIEWS,
-  reviews
-})
-
-export const receiveReview = review => ({
-  type: RECEIVE_REVIEW,
-  review
-})
-
-export const removeReview = reviewId => ({
-  type: REMOVE_REVIEW,
-  reviewId
-})
+import {receiveBook} from './book_actions';
 
 export const fetchReviews = (bookId) => dispatch => (
-  ReviewAPIUtil.fetchReviews(bookId).then(reviews => dispatch(receiveReviews(reviews)))
+  ReviewAPIUtil.fetchReviews(bookId).then(book => dispatch(receiveBook(book)))
 )
 
 export const fetchReview = (review) => dispatch => (
-  ReviewAPIUtil.fetchReview(review).then(review => dispatch(receiveReview(review)))
+  ReviewAPIUtil.fetchReview(review).then(book => dispatch(receiveBook(book)))
 )
 
 export const createReview = review => dispatch => (
-  ReviewAPIUtil.createReview(review).then(review => dispatch(receiveReview(review)))
+  ReviewAPIUtil.createReview(review).then(book => dispatch(receiveBook(book)))
 )
 
 export const deleteReview = review => dispatch => (
-  ReviewAPIUtil.deleteReview(review).then(reviewId => dispatch(removeReview(review.id)))
+  ReviewAPIUtil.deleteReview(review).then(book => dispatch(receiveBook(book)))
 )
