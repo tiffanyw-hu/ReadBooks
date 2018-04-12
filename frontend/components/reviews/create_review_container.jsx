3a94c13a15c1b585c13402e9db71d1a3d239dc0c
Andrew Tae <tiffanyw.hu@gmail.com>
2018-04-11 19:37:53 -0700

    Finish modal for edit and create review

--- frontend/components/reviews/create_review_container.jsx
+++ frontend/components/reviews/create_review_container.jsx
@@ -4,10 +4,10 @@ import {createReview} from '../../actions/review_actions';
 import ReviewForm from './review_form';
 
 const mapStateToProps = (state, ownProps) => {
+  let book = ownProps.book
   let formType = "Write a Review"
-  console.log(ownProps)
   let review = {body: "", rating: null, book_id: ownProps.bookId }
-  return {review, formType}
+  return {review, formType, book}
 }
 
 const mapDispatchToProps = dispatch => ({
--- frontend/components/reviews/edit_review_container.jsx
+++ frontend/components/reviews/edit_review_container.jsx
@@ -4,10 +4,11 @@ import {fetchReview, updateReview} from '../../actions/review_actions';
 import ReviewForm from './review_form';
 
 const mapStateToProps = (state, ownProps) => {
+  let book = ownProps.book
   let formType = "Edit Review"
   let review = ownProps.review
   let bookId = ownProps.bookId
-  return {review, formType, bookId};
+  return {review, formType, bookId, book};
 }
 
 const mapDispatchToProps = dispatch => ({
