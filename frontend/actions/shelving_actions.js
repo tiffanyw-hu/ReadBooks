5c2c2baae0dd6edd19569f33fbe7f14ec22325f1
Andrew Tae <tiffanyw.hu@gmail.com>
2018-04-12 21:39:41 -0700

    I can't believe that worked

--- frontend/actions/shelving_actions.js
+++ frontend/actions/shelving_actions.js
@@ -1,10 +1,10 @@
 import * as ShelvingAPIUtil from '../utils/shelving_api_util';
-import {receiveShelf, RECEIVE_SHELF} from './shelf_actions';
+import {receiveBook} from './book_actions';
 
 export const createShelving = shelving => dispatch => (
-  ShelvingAPIUtil.createShelving(shelving).then(shelf => dispatch(receiveShelf(shelf)))
+  ShelvingAPIUtil.createShelving(shelving).then(book => dispatch(receiveBook(book)))
 )
 
 export const deleteShelving = id => dispatch => (
-  ShelvingAPIUtil.deleteShelving(id).then(shelf => dispatch(receiveShelf(shelf)))
+  ShelvingAPIUtil.deleteShelving(id).then(book => dispatch(receiveBook(book)))
 )
